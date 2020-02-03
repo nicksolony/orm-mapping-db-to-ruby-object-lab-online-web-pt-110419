3aadd07cad7dbfb2249121e3a5b4326b0310e05f
Nick Solonyy <nicksolony@gmail.com>
2020-02-03 04:16:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,18 @@ class Student
     self.new_from_db(row)
   end
   
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+              SELECT * FROM students
+              WHERE name = ?
+              LIMIT 1;
+              SQL
+    row = DB[:conn].execute(sql,name).first
+    self.new_from_db(row)
+  end
+  
   
   
   
